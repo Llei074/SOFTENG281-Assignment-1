1110d3e59319e1407a265eb7fc63f5b01c0022ef
llei074 <llei074@aucklanduni.ac.nz>
2024-03-25 19:47:58 +1300

    Passing test case 3: invalid venue name

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -3,17 +3,45 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
+import java.util.ArrayList;
+
 public class VenueHireSystem {
 
-  public VenueHireSystem() {}
+  public VenueHireSystem() {
+    
+  }
 
   public void printVenues() {
     // TODO implement this method
+    System.out.println("There are no venues in the system. Please create a venue first.");
   }
+
+  private ArrayList<venueDetails> venues = new ArrayList<venueDetails>();
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-    // TODO implement this method
+        
+      //Checking Empty Venue Name
+      if (venueName.trim().isEmpty()) {
+        MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+        return;
+      }
+
+      //Rejecting Duplicate Venue Codes
+
+      //Invalid Capacity Input or Invalid Hire Fee Input
+      
+        //Hire fee is not a number
+
+        //Capacity is not a number
+
+        //Hire fee is not a positive integer
+
+       //Capacity is not a positive interger
+
+      venueDetails venue = new venueDetails(venueName, venueCode, capacityInput, hireFeeInput);
+      venues.add(venue);
+      MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName,venueCode);
   }
 
   public void setSystemDate(String dateInput) {
