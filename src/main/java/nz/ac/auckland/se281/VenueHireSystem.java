4493ba274b2f2a76af6854318f034cbf160f87e6
llei074 <llei074@aucklanduni.ac.nz>
2024-03-26 18:58:40 +1300

    Invalid capacity and hire fees input code working with MainTest and personal test cases

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -15,14 +15,14 @@ public class VenueHireSystem {
 
   public void printVenues() {
     if (venues.isEmpty() == true) { // Checks if the venue array list is empty
-      System.out.println("There are no venues in the system. Please create a venue first.");
+      MessageCli.NO_VENUES.printMessage(); // Currently no venues
       return;
     } else if (venues.size() == 1) { // Sentence format for one venue
-      MessageCli.NUMBER_VENUES.printMessage("is", IntToWord[venues.size()], "", ":");
+      MessageCli.NUMBER_VENUES.printMessage("is", IntToWord[venues.size()], "");
     } else if (venues.size() < 10) { // Sentence format for more than one venue
-      MessageCli.NUMBER_VENUES.printMessage("are", IntToWord[venues.size()], "s", ":");
+      MessageCli.NUMBER_VENUES.printMessage("are", IntToWord[venues.size()], "s");
     } else {
-      MessageCli.NUMBER_VENUES.printMessage("are", String.valueOf(venues.size()), "s", ":");
+      MessageCli.NUMBER_VENUES.printMessage("are", String.valueOf(venues.size()), "s");
     }
     for (venueDetails venue : venues) {
       MessageCli.VENUE_ENTRY.printMessage(
@@ -40,7 +40,7 @@ public class VenueHireSystem {
     }
 
     // Rejecting Duplicate Venue Codes
-    if (venues.isEmpty() != true) {
+    if (venues.isEmpty() != true) { // This will skip if there are no venues in the venues array
       for (venueDetails venue : venues) {
         if (venue.getCode().equals(venueCode)) {
           MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue.getName());
@@ -50,14 +50,24 @@ public class VenueHireSystem {
     }
 
     // Invalid Capacity Input or Invalid Hire Fee Input
-
-    // Hire fee is not a number
-    
-    // Capacity is not a number
-
-    // Hire fee is not a positive integer
-
-    // Capacity is not a positive interger
+    try {
+      if (Integer.parseInt(hireFeeInput) <= 0) { // Hire fee is not a positive integer or zero
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
+        return;
+      }
+    } catch (NumberFormatException nfe) { // Hire fee is not a number
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
+      return;
+    }
+    try {
+      if (Integer.parseInt(capacityInput) <= 0) { // Capacity is not a positive integer or zero
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
+        return;
+      }
+    } catch (NumberFormatException nfe) { // capacity is not a number
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "");
+      return;
+    }
 
     venueDetails venue = new venueDetails(venueName, venueCode, capacityInput, hireFeeInput);
     venues.add(venue);
