bdcc415b6ada55dd93d838e7d424667617fb5736
llei074 <llei074@aucklanduni.ac.nz>
2024-04-06 19:28:51 +1300

    Updated class name to meet codestyle

--- src/main/java/nz/ac/auckland/se281/VenueDetails.java
+++ src/main/java/nz/ac/auckland/se281/VenueDetails.java
@@ -1,13 +1,13 @@
 package nz.ac.auckland.se281;
 
-public class venueDetails {
+public class VenueDetails {
 
   private String name;
   private String code;
   private String capacity;
   private String hirefee;
 
-  public venueDetails(String name, String code, String capacityInput, String hireFeeInput) {
+  public VenueDetails(String name, String code, String capacityInput, String hireFeeInput) {
     this.name = name;
     this.code = code;
     this.capacity = capacityInput;
